30ac8a2670fe40da5b4cdad06a54f40d6beea250
Jakob <shavinski.jakob@gmail.com>
2023-07-12 12:42:49 -0700

    updated new image source to modern soruce webp

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,7 +1,10 @@
+// import jobly-img.webp from '../images'
+import jobly from '../images/jobly-img.webp'
+import twitter from '../images/twitter-verified-pattern.webp'
 
 const projects = [
     {   
-        image: "https://getfive.com/wp-content/uploads/2019/08/job-searching.jpg",
+        image: `${jobly}`,
         title: 'Jobly',
         description: 'A job searching app where users can login, search for companies, and search for open jobs!',
         note: 'Note that I am using a free online cloud database so my projects will require 10-30 seconds to load upon start',
@@ -10,7 +13,7 @@ const projects = [
     },
 
     {
-        image: "https://eletiofe.com/wp-content/uploads/2022/12/339872-twitter-blue-explained-what-is-it-how-much-does-it-cost.jpg",
+        image: `${twitter}`,
         title: 'Warbler',
         description: 'A twitter clone which allows users to login, make posts, and follow other users!',
         note: 'Note that I am using a free online cloud database so my projects will require 10-30 seconds to load upon start',

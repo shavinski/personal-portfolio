d9b0b3ad15f246ba3cb3dbfc62406a61ece802b4
Jakob <shavinski.jakob@gmail.com>
2023-07-13 20:14:32 -0700

    Update Jobly live site link

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -8,7 +8,7 @@ const projects = [
         title: 'Jobly',
         description: 'A job searching app where users can login, search for companies, and search for open jobs!',
         note: 'Note that I am using a free online cloud database so my projects will require 10-30 seconds to load upon start',
-        website: "https://shavinski-joblyfrontend.surge.sh",
+        website: "http://jobly.shavinski.surge.sh/",
         github: "https://github.com/shavinski/react-jobly",
     },
 

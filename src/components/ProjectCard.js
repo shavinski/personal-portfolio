33cd39052ba031f595130c9274b0d077beca5773
Jakob <shavinski.jakob@gmail.com>
2023-08-22 10:59:49 -0700

    Refactor HTML
    Get rid of button inside a tags

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,17 +2,17 @@ import './ProjectCard.css'
 import { motion } from "framer-motion";
 
 function ProjectCard({ project }) {
-  
+
     const cardImgStyle = {
         backgroundImage: `url(${project.image})`
     }
 
     return (
         <motion.div
-        initial={{ opacity: 0, x: -50 }}
-        whileInView={{ opacity: 1,  x: 0  }}
-        transition={{ ease: "easeInOut", duration: .7 }}
-        viewport={{ once: true }}
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ ease: "easeInOut", duration: .7 }}
+            viewport={{ once: true }}
             className='project-card'>
             <div className='card-img lazyload' style={cardImgStyle}></div>
             <div className='card-info'>
@@ -22,8 +22,8 @@ function ProjectCard({ project }) {
                     <p className='note'>{project.note}</p>
                 }
                 <div className='link-container'>
-                    <a className='web-link' href={project.website} aria-label='Link for project website' target='blank'><button className='card-btn'>Live</button></a>
-                    <a className='web-link' href={project.github} aria-label='Link for project github' target='blank'><button className='card-btn'>Code</button></a>
+                    <a className='card-btn' href={project.website} aria-label='Link for project website' target='blank'>Live</a>
+                    <a className='card-btn' href={project.github} aria-label='Link for project github' target='blank'>Code</a>
                 </div>
             </div>
         </motion.div>

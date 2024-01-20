6573e894c94c819999be6ffc8acd1f5eecd264c7
Jakob <shavinski.jakob@gmail.com>
2024-01-20 03:16:29 -0800

    Remove non necessary information

--- src/assets/socials.js
+++ src/assets/socials.js
@@ -1,4 +1,3 @@
-import resume from '../images/Resume.pdf'
 
 const socials = [
     {
@@ -17,37 +16,10 @@ const socials = [
                 <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
             </svg>,
         alt: `Jakob's linkedin`
-    },
-
-    {
-        link: `${resume}`,
-        svg:
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#FFFFFF" className="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
-                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z" />
-            </svg>,
-        alt: `Jakob's resume`
-    },
+    }
 ]
 
 const contactInfo = [
-    {
-        title: "Call/Text",
-        info: "(805) 813 7395",
-        svg:
-            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#FFFF00" className="bi bi-phone-fill" viewBox="0 0 16 16">
-                <path d="M3 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V2zm6 11a1 1 0 1 0-2 0 1 1 0 0 0 2 0z" />
-            </svg>
-    },
-
-    {
-        title: "Email",
-        info: "shavinski.jakob@gmail.com",
-        svg:
-            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#FFFF00" className="bi bi-envelope-fill" viewBox="0 0 16 16">
-                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
-            </svg>
-    },
-
     {
         title: "Location",
         info: "Los Angeles, CA",
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,6 @@
 import './AboutMe.css'
 import EngineerCard from './EngineerCard';
 import jobs from '../assets/jobs';
-import resume from '../images/Resume.pdf'
 import { v4 as uuidv4 } from 'uuid';
 
 import { motion } from "framer-motion";
@@ -59,18 +58,6 @@ function AboutMe() {
                             svg={job.svg} />
                     ))}
                 </div>
-
-                <a className='resume-link' target='blank' href={resume}>
-                    <motion.button
-                        initial={{ opacity: 0, x: -50 }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        transition={{ ease: "easeInOut", duration: .7 }}
-                        viewport={{ once: true }}
-                        className='button-resume slide-right'><p>Resume</p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
-                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
-                            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
-                        </svg></motion.button>
-                </a>
             </div>
         </section>
     )
